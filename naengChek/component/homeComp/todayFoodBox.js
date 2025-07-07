88a671b891674fca03cb1e75f1f14b58aaf7392d
20211896 <20211896@edu.hanbat.ac.kr>
2025-07-07 11:21:27 +0900

    feat: 홈화면 오늘의 추천 요리 요리박스 컴포넌트 생성

--- naengChek/component/homeComp/todayFoodBox.js
+++ naengChek/component/homeComp/todayFoodBox.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import Btn_1_4 from '../btnStyle/size1-4';
 
 export default function TodayFoodBox({ navigation }) {
     return (
         // TODO: 데이터 연결하기
         <View style={styles.food}>
-            <Image style={styles.foodImg} source={{ uri: 'https://picsum.photos/80/80' }}/>
+            <Image style={styles.foodImg} source={{ uri: 'https://picsum.photos/80/80' }} />
 
             <View style={styles.foodText}>
                 <Text>요리명</Text>
-                <Text>재료</Text>
+                <View style={styles.ingredients}>
+                    <Btn_1_4 title="재료" fontSize={10} onPress={() => { }} />
+                    <Btn_1_4 title="재료" fontSize={10} onPress={() => { }} />
+                </View>
                 <Text>설명</Text>
             </View>
         </View>
@@ -17,20 +21,30 @@ export default function TodayFoodBox({ navigation }) {
     );
 } const styles = StyleSheet.create({
     food: {
-        flexDirection: 'row',
         paddingHorizontal: 16,
         paddingVertical: 19,
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        borderRadius: 5,
 
         //HACK : 레이아웃 확인용
-        backgroundColor: "#dadada"
+        borderWidth: 1,
+        borderColor: '#000000',
     },
     foodImg: {
-        width: '24%',         
-        aspectRatio: 1,       
-        marginRight: 13,      
+        width: '24%',
+        aspectRatio: 1,
+        marginRight: 13,
     },
     foodText: {
         flexDirection: 'column',
+        justifyContent: 'space-between',
+        flex: 1,
+    },
+    ingredients: {
+        flexDirection: 'row',
+        gap: 4,
+        flexWrap: 'wrap',
     }
 });
 
