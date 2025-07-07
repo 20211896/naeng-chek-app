ef68d835ddefbd95cfd47e05ac3f98c5e5dab1d7
20211896 <20211896@edu.hanbat.ac.kr>
2025-07-07 14:16:15 +0900

    feat: 식재료 페이지 생성

--- naengChek/App.js
+++ naengChek/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './page/home';     // 실제 경로에 맞게 조정
-import MyPage from './page/myPage.js'; // 필요한 다른 페이지도 추가
+import Home from './page/home';    
+import Ingredient from './page/ingredient';    
+import MyPage from './page/myPage.js'; 
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Ingredient" component={Ingredient} />
         <Stack.Screen name="MyPage" component={MyPage} />
       </Stack.Navigator>
     </NavigationContainer>
