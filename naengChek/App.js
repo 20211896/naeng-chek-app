3ef3e31b1cd03df6c1d45a1de0d9fdc94bcb3615
20211896 <20211896@edu.hanbat.ac.kr>
2025-07-10 22:04:35 +0900

    feat: react-native-reanimated 환경설정

--- naengChek/App.js
+++ naengChek/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import Home from './page/home';    
-import Ingredient from './page/ingredient';    
-import MyPage from './page/myPage'; 
-import Login from './page/login'; 
-import IdFind from './page/idFind'; 
-
-
+import Home from './page/home';
+import Ingredient from './page/ingredient';
+import MyPage from './page/myPage';
+import Login from './page/login';
+import IdFind from './page/idFind';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="IdFind" component={IdFind} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Ingredient" component={Ingredient} />
-        <Stack.Screen name="MyPage" component={MyPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Ingredient" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="IdFind" component={IdFind} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Ingredient" component={Ingredient} />
+          <Stack.Screen name="MyPage" component={MyPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
---
+++ naengChek/component/ingredComp/ingredSortBox.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width * 0.3;
+
+export default function ingredSortBox({ navigation }) {
+    return (
+        <View style={styles.ingred}>
+
+            <View style={styles.ingredTextBox}>
+                <View style={styles.ingredText}>
+                    <Text style={styles.ingredTextItem}>재료</Text>
+                    <View style={styles.explainText}>
+                        <Text style={styles.explainTextItem}>보관장소</Text>
+                        <Text style={styles.explainTextItem}>유통기한</Text>
+                    </View>
+                </View>
+                <Text style={styles.ingredTextItem}>수량</Text>
+            </View>
+        </View>
+
+    );
+} const styles = StyleSheet.create({
+    ingred: {
+        padding: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+
+        //HACK : 레이아웃 확인용
+        borderWidth: 1,
+        borderColor: '#000000',
+    },
+    ingredImg: {
+        width: imageSize,
+        height: imageSize,
+        marginRight: 13,
+    },
+    ingredTextBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 1,
+    },
+    ingredText: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        height: imageSize,
+    },
+    explainText: {
+        flexDirection: 'row',
+        gap: 15,
+    },
+    explainTextItem: {
+        fontSize: 14,
+        color: '#929292',
+    },
+    ingredTextItem: {
+        fontSize: 15,
+        fontWeight: 550
+    },
+});
+
